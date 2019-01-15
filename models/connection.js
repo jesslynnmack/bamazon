d927cabd7ecf7b54158d92a5616f07c84eb665a0
Jessica Mccormack <jesslynn.mack@gmail.com>
2019-01-15 13:37:33 -0500

    added JawsDB info

--- models/connection.js
+++ models/connection.js
@@ -1,16 +1,16 @@
-// var mysql = require('mysql');
-// var connection;
+var mysql = require('mysql');
+var connection;
 
-// if(process.env.JAWSDB_URL){
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }else{
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password:'Jm46149!',
-//         database: 'bamazon'
-//     });
-// };
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'Jm46149!',
+        database: 'bamazon'
+    });
+};
 
-// connection.connect();
-// module.exports = connection;
+connection.connect();
+module.exports = connection;
